cbdd7675f0ef1b55ee06cb45930a786d71a31ad5
Tejas Adhikari <tejasadhikari123@gmail.com>
2024-12-09 01:59:53 +0530

    completed till Update questions

--- Kanbas/Questions/routes.js
+++ Kanbas/Questions/routes.js
@@ -10,7 +10,7 @@ export default function QuestionRoutes(app) {
     app.put("/api/questions/:questionId", async (req, res) => {
         const { questionId } = req.params;
         const questionUpdates = req.body;
-        const status = await quizzesDao.updateQuiz(questionId, questionUpdates);
+        const status = await questionsDao.updateQuestion(questionId, questionUpdates);
         res.send(status);
     });    
 }
